791f78bb7fae472244627d16e5c9f2f8c55a5fd5
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-06-10 14:21:32 +0300

    Done Task_1

---
+++ Task_1.py
@@ -0,0 +1,10 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+base_1 = 5 # основание
+power_1 = 5 #назначаем степень
+
+def Exponentiation(base, power):
+    if power == 1:
+        return base
+    return  base * Exponentiation(base, power -1)
+print(Exponentiation(3,4))
